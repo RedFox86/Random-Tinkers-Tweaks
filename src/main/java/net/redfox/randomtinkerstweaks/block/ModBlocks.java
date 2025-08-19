5676afdbe7f2933d92530599f92c00d3b97293d5
Peter <85247098+RedFox86@users.noreply.github.com>
2025-08-18 20:29:50 -0400

    Change to create properties on load
    Instead of copying from Scorched Alloyer, which sometimes isn't initialized yet.

--- src/main/java/net/redfox/randomtinkerstweaks/block/ModBlocks.java
+++ src/main/java/net/redfox/randomtinkerstweaks/block/ModBlocks.java
@@ -6,6 +6,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,7 +15,7 @@ import net.minecraftforge.registries.RegistryObject;
 import net.redfox.randomtinkerstweaks.RandomTinkersTweaks;
 import net.redfox.randomtinkerstweaks.block.custom.SearedAlloyerBlock;
 import net.redfox.randomtinkerstweaks.item.ModItems;
-import slimeknights.tconstruct.smeltery.TinkerSmeltery;
+import slimeknights.tconstruct.smeltery.block.component.SearedBlock;
 
 public class ModBlocks {
   public static final DeferredRegister<Block> BLOCKS =
@@ -24,8 +26,16 @@ public class ModBlocks {
           "seared_alloyer",
           () ->
               new SearedAlloyerBlock(
-                  BlockBehaviour.Properties.copy(TinkerSmeltery.scorchedAlloyer.get())
-                      .sound(SoundType.METAL)));
+                  BlockBehaviour.Properties.of()
+                      .mapColor(MapColor.COLOR_GRAY)
+                      .sound(SoundType.METAL)
+                      .instrument(NoteBlockInstrument.BASEDRUM)
+                      .requiresCorrectToolForDrops()
+                      .strength(3.0F, 9.0F)
+                      .isValidSpawn(
+                          (s, r, p, e) ->
+                              !s.hasProperty(SearedBlock.IN_STRUCTURE)
+                                  || !(Boolean) s.getValue(SearedBlock.IN_STRUCTURE))));
 
   public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
     RegistryObject<T> toReturn = BLOCKS.register(name, block);
